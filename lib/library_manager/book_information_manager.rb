7f4000bc22f132569b0f23a31e3c72a63570bebd
Andre LaFleur <andre.lafleur@utah.edu>
2016-07-14 21:37:26 -0600

    Trim unnecessary helper methods

--- lib/library_manager/book_information_manager.rb
+++ lib/library_manager/book_information_manager.rb
@@ -192,69 +192,8 @@ class BookInformationManager
 
   private
 
-  # Helper method. Returns all the authors associated with a given book, or false if
-  # the book doesn't have all of the expected last or first names provided.
-  def get_all_authors(expect_auth_last, expect_auth_first, book)
-    authors = Author.all(:book => book)
-
-    if expect_auth_last
-      verify_last = {}
-
-      expect_auth_last.each do |auth|
-        verify_last[auth] = false
-      end
-
-      authors.each do |auth|
-        verify_last[auth.last_name] = true
-      end
-
-      verify_last.each_value do |val|
-        return false unless val
-      end
-    end
-
-    if expect_auth_first
-      verify_first = {}
-
-      expect_auth_first.each do |auth|
-        verify_first[auth] = false
-      end
-
-      authors.each do |auth|
-        verify_first[auth.first_name] = true
-      end
-
-      verify_first.each_value do |val|
-        return false unless val
-      end
-    end
-
-    authors
-  end
-
   # Helper method. Returns all the subjects associated with a given book, or false
   # if the book doesn't have all of the expected subjects provided
-  def get_all_subjects(expected_subjects, book)
-    subjects = Subject.all(:book => book)
-
-    if expected_subjects
-      verify = {}
-
-      expected_subjects.each do |sub|
-        verify[sub] = false
-      end
-
-      subjects.each do |sub|
-        verify[sub.subject] = true
-      end
-
-      verify.each do |sub, value|
-        return false unless value
-      end
-    end
-
-    subjects
-  end
 
   def match_all_titles?(titles, book)
     add_book = true
@@ -328,21 +267,6 @@ class BookInformationManager
     subjects.each do |subject|
       return true if expected_subjects && expected_subjects.include?(subject.subject)
     end
-
-    false
-  end
-
-  # Helper method. Returns true if given the book provided should be added to the collection that will be returned by
-  # the query, given whether or not the user desires a checked out book.
-  def checked_out_book_include?(want_checked_out, book)
-    borrower = Borrower.last(:book => book)
-    return true if !want_checked_out && !borrower
-    return false if want_checked_out && !borrower
-
-    is_checked_out = borrower.attribute_get(:date_returned) == nil
-
-    return true if want_checked_out && is_checked_out
-    return true if !want_checked_out && !is_checked_out
 
     false
   end
